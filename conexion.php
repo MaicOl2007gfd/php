acfe4f0d5e17f8938c5e9b85d878d85557264945
MaicOl2007gfd <maicolquevedo29@gmail.com>
2026-03-25 01:53:17 -0500

    onnection code
    Remove database connection code from conexion.php

--- conexion.php
+++ conexion.php
@@ -1,13 +1,1 @@
-<?php
-$server = "localhost";
-$user = "root";
-$password= "";
-$db="sopas";
-$conn = new mysqli($server,$user,$password,$db);
 
-if ($conn->connect_error){
-    die("Error de conexion: " .$conn->connect_error);
-}
-
-
-?>
